be377def7e94bba66cb2e019e50c5471a4f1d7b1
Dani Coleman <danicoleman00@gmail.com>
2021-05-03 16:42:47 -0600

    Add resources for all crud actions for a photo

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :photos
+  # get "/photos", to: "photos#index"
+  # get "/photos/new", to: "photos#new"
 end
