c08562f024853dc1bb7e840425718cfbd477f4ed
Dani Coleman <danicoleman00@gmail.com>
2021-05-03 17:45:59 -0600

    Delete comments

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe User, type: :model do
   describe  'validations' do
     it { should validate_presence_of(:name) }
   end
-
-  describe 'relationships' do
-    it { should have_many :photos }
-  end
 end
